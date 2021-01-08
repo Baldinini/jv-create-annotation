a153ba9b1eadec83f72f01f78d7b330dcece6d47
Baldinini <baldinat063@gmail.com>
2021-01-08 13:49:16 +0200

    bugs fixed

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -6,7 +6,7 @@ import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-    BetDao betDao = new BetDaoImpl();
+    private static final BetDao betDao = new BetDaoImpl();
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
@@ -22,11 +22,13 @@ public class ConsoleHandler {
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
                 bet = new Bet(value, risk);
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please, enter correct data");
             }
-            betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
+            if (bet != null) {
+                betDao.add(bet);
+                System.out.println(bet);
+            }
         }
     }
 }
